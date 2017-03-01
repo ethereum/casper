d7ac0028fa6d14922f0a539a7062bc95319db5d4
Jan Xie <jan.h.xie@gmail.com>
2017-03-01 17:39:44 +0800

    scaffold

---
+++ setup.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import codecs
+from setuptools import setup
+from setuptools.command.test import test as TestCommand
+
+
+class PyTest(TestCommand):
+    def __init__(self, *args, **kwargs):
+        TestCommand.__init__(self, *args, **kwargs)
+        self.test_suite = True
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+
+    def run_tests(self):
+        # import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.test_args)
+        raise SystemExit(errno)
+
+
+INSTALL_REQUIRES_REPLACEMENTS = {}
+INSTALL_REQUIRES = list()
+with open('requirements.txt') as requirements_file:
+    for requirement in requirements_file:
+        dependency = INSTALL_REQUIRES_REPLACEMENTS.get(
+            requirement.strip(),
+            requirement.strip(),
+        )
+
+        INSTALL_REQUIRES.append(dependency)
+
+INSTALL_REQUIRES = list(set(INSTALL_REQUIRES))
+
+# *IMPORTANT*: Don't manually change the version here. Use the 'bumpversion' utility.
+# see: https://github.com/ethereum/pyethapp/wiki/Development:-Versions-and-Releases
+version = '0.0.1'
+
+setup(
+    name='simplecasper',
+    version=version,
+    description='Ethereum Casper POC4 Daemon',
+    long_description="Ethereum Casper POC4 Daemon",
+    author='Jan Xie',
+    author_email='jan.h.xie@gmail.com',
+    url='https://github.com/cryptape/simplecasper',
+    packages=[
+        'simplecasper',
+    ],
+    package_data={},
+    license='MIT',
+    zip_safe=False,
+    keywords=[
+        'ethereum',
+        'consensus',
+        'casper'
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+    ],
+    cmdclass={'test': PyTest},
+    install_requires=INSTALL_REQUIRES,
+    tests_require=[],
+    entry_points='''
+    [console_scripts]
+    simplecasper=simplecasper.app:app
+    '''
+)
---
+++ simplecasper/__init__.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+# ############# version ##################
+from pkg_resources import get_distribution, DistributionNotFound
+import os.path
+import subprocess
+import re
+
+
+GIT_DESCRIBE_RE = re.compile('^(?P<version>v\d+\.\d+\.\d+)-(?P<git>\d+-g[a-fA-F0-9]+(?:-dirty)?)$')
+
+
+__version__ = None
+try:
+    _dist = get_distribution('simplecasper')
+    # Normalize case for Windows systems
+    dist_loc = os.path.normcase(_dist.location)
+    here = os.path.normcase(__file__)
+    if not here.startswith(os.path.join(dist_loc, 'simplecasper')):
+        # not installed, but there is another version that *is*
+        raise DistributionNotFound
+    __version__ = _dist.version
+except DistributionNotFound:
+    pass
+
+if not __version__:
+    try:
+        rev = subprocess.check_output(['git', 'describe', '--tags', '--dirty'],
+                                      stderr=subprocess.STDOUT)
+        match = GIT_DESCRIBE_RE.match(rev)
+        if match:
+            __version__ = "{}+git-{}".format(match.group("version"), match.group("git"))
+    except:
+        pass
+
+if not __version__:
+    __version__ = 'undefined'
+
+# ########### endversion ##################
