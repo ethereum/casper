db4147db5577d7f2dddf0ede2fd74a0fa24cc6cb
Karl Floersch <karl@karlfloersch.com>
2017-03-29 14:42:48 +0200

    fix tmux.sh to have previous behavior

--- tmux.sh
+++ tmux.sh
@@ -5,22 +5,22 @@ tmux start-server;
 TMUX= tmux new-session -d -s simplecasper -n casper-daemons
 
 tmux send-keys -t simplecasper:0.0 source\ \~/.zshrc C-m
-tmux send-keys -t simplecasper:0.0 simplecasper\ -l\ casper:debug\ run\ 0 C-m
+tmux send-keys -t simplecasper:0.0 simplecasper\ -l\ casper:debug\ -d\ data0\ run\ 0\ --fake-account C-m
 
 tmux splitw -t simplecasper:0
 tmux select-layout -t simplecasper:0 tiled
 tmux send-keys -t simplecasper:0.1 source\ \~/.zshrc C-m
-tmux send-keys -t simplecasper:0.1 simplecasper\ -l\ casper:debug\ run\ 1 C-m
+tmux send-keys -t simplecasper:0.1 simplecasper\ -l\ casper:debug\ -d\ data1\ run\ 1\ --fake-account C-m
 
 tmux splitw -t simplecasper:0
 tmux select-layout -t simplecasper:0 tiled
 tmux send-keys -t simplecasper:0.2 source\ \~/.zshrc C-m
-tmux send-keys -t simplecasper:0.2 simplecasper\ -l\ casper:debug\ run\ 2 C-m
+tmux send-keys -t simplecasper:0.2 simplecasper\ -l\ casper:debug\ -d\ data2\ run\ 2\ --fake-account C-m
 
 tmux splitw -t simplecasper:0
 tmux select-layout -t simplecasper:0 tiled
 tmux send-keys -t simplecasper:0.3 source\ \~/.zshrc C-m
-tmux send-keys -t simplecasper:0.3 simplecasper\ -l\ casper:debug\ run\ 3 C-m
+tmux send-keys -t simplecasper:0.3 simplecasper\ -l\ casper:debug\ -d\ data3\ run\ 3\ --fake-account C-m
 
 tmux select-layout -t simplecasper:0 tiled
 tmux select-pane -t simplecasper:0.0
